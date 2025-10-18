981056c0a5558bb4e0c2102bf1c758937f9e6834
Matloop <matheusdiasestacioo@gmail.com>
2025-10-18 15:07:24 -0300

    new astro version installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,23 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: vercel(),
+
   server: {
     host: 'localhost', // Garante que o servidor use 'localhost' em vez de '127.0.0.1' ou um IP de rede
     port: 3000,        // Define a porta padrão para 3000
   },
+
   vite: {
     ssr: {
       noExternal: ['gsap']
     }
-  }
+  },
+
+  adapter: vercel()
 });
